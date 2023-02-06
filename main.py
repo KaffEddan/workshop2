48bbaba10f0bde45937bc09f77ae5debbefe98fc
Viggez <115564652+Viggez@users.noreply.github.com>
2023-02-06 12:08:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     name = input("Please input your name: ")
     print(f'Hello {name}!')
-    print("My name is George!")
+    print("My name is Jim!")
 
 if __name__ == '__main--':
     main()
